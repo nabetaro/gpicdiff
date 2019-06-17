6ffd7ae81cbf21ba1fee3e9cf5b07db6c5b1e139
KURASAWA Nozomu <nabetaro@caldron.jp>
2019-06-17 20:08:13 +0900

    add external diff script for git.

--- contrib/git-gpicdiff.sh
+++ contrib/git-gpicdiff.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-gpicdiff --label-path1=${1}:${3} --label-path2=${1}:${6} $2 $5 
+hash1=`echo ${3} | cut -c 1-7`
+hash2=`echo ${6} | cut -c 1-7`
+
+gpicdiff --label-path1=${1}:${hash1} --label-path2=${1}:${hash2} $2 $5
