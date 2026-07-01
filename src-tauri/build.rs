f256fedaf642572285c2e51a2e31b919262aab7b
KURASAWA Nozomu <kurasawa.nozomu@keiken.co.jp>
2026-07-01 10:40:07 +0900

    Fix build informations.

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
 fn main() {
+    // compress changelog
+    Command::new("gzip")
+        .args([
+            "-9", "-k", "-f",
+            "../debian/changelog",
+        ])
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .status()
+        .expect("compressing changelog: failed.");
+
     tauri_build::build()
 }
